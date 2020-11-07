4031cb54a36aed3cdb87f262b6ffe1c75e9dc9d6
iigmir <roc120j@gmail.com>
2020-11-07 23:56:31 +0800

    ???

--- src/component-plugin.js
+++ src/component-plugin.js
@@ -11,15 +11,53 @@ export default
         })
         {
             const Profile = Vue.extend({
-                render(h) {
+                render(h)
+                {
                     const dom = options.dom || "div";
                     return h( dom, {
                         id: options.id || "",
-                        class: options.class.split(" ") || "",
+                        class: options.class ? options.class.split(" ") : "",
                     }, options.text || "" );
                 },
+                mounted()
+                {
+                },
+                methods: {
+                    once()
+                    {
+                                            // const handleDestroy = () =>
+                    // {
+                    //     this.$nextTick(() =>
+                    //     {
+                    //         const request_af =
+                    //             window.requestAnimationFrame ||
+                    //             window.webkitRequestAnimationFrame ||
+                    //             window.mozRequestAnimationFrame ||
+                    //             window.msRequestAnimationFrame ||
+                    //             window.oRequestAnimationFrame ||
+                    //             (cb => setTimeout(cb, 16));
+                    //         request_af(() => {
+                    //             this.$destroy()
+                    //         })
+                    //     })
+                    // };
+                    // this.$parent.$once("hook:destroyed", handleDestroy);
+                    // this.$once("hidden", handleDestroy);
+                    // this.show();
+                    }
+                },
+                destroyed()
+                {
+                    if (this.$el && this.$el.parentNode)
+                    {
+                      this.$el.parentNode.removeChild(this.$el);
+                    }
+                },
             });
-            return Profile;
+            const div = document.createElement("div");
+            document.body.appendChild(div);
+            new Profile().$mount(div);
+            return true;
         }
     }
 };
