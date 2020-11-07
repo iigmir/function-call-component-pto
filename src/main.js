ae66c5a27fb02b936aa414cc79625197be803629
iigmir <roc120j@gmail.com>
2020-11-07 13:35:40 +0800

    Add plugin

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
-import MyPlugin from './my-vue-plugin2.js';
+import MyPlugin from './component-plugin.js';
 import App from './App.vue'
 
 Vue.use(MyPlugin)
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue({ render: h => h(App), }).$mount('#app')
