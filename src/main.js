9d07bb1387b1097b7fe06b932738b8d1751405f8
iigmir <roc120j@gmail.com>
2020-11-10 22:57:03 +0800

    linting

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import Vue from 'vue'
-import MyPlugin from './component-plugin.js';
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import FunctionalPlugin from "./component-plugin.js";
 
-Vue.use(MyPlugin)
-
-Vue.config.productionTip = false
+Vue.use(FunctionalPlugin);
+Vue.config.productionTip = false;
 
 new Vue({ render: h => h(App), }).$mount('#app')
